341a9e7a52ecd8dc232fdf741a90ed25c031ee83
fmbxnary <88243935+fmbxnary@users.noreply.github.com>
2022-01-06 13:49:54 +0300

    traverse gets parameter

--- tree-avl/src/main/java/com/youtube/geekific/AVLTree.java
+++ tree-avl/src/main/java/com/youtube/geekific/AVLTree.java
@@ -78,15 +78,35 @@ public class AVLTree<T extends Comparable<T>> implements Tree<T> {
     }
 
     @Override
-    public void traverse() {
-        traverseInOrder(root);
+    public void traverse(String order) {
+        switch (order) {
+            case "inorder" -> traverseInorder(root);
+            case "preorder" -> traversePreorder(root);
+            case "postorder" -> traversePostorder(root);
+        }
     }
 
-    private void traverseInOrder(Node<T> node) {
+    private void traverseInorder(Node<T> node) {
         if (node != null) {
-            traverseInOrder(node.getLeftChild());
-            System.out.println(node);
-            traverseInOrder(node.getRightChild());
+            traverseInorder(node.getLeftChild());
+            System.out.println(node.getData());
+            traverseInorder(node.getRightChild());
+        }
+    }
+
+    private void traversePreorder(Node<T> node) {
+        if (node != null) {
+            System.out.println(node.getData());
+            traversePreorder(node.getLeftChild());
+            traversePreorder(node.getRightChild());
+        }
+    }
+
+    private void traversePostorder(Node<T> node) {
+        if (node != null) {
+            traversePreorder(node.getLeftChild());
+            traversePostorder(node.getRightChild());
+            System.out.println(node.getData());
         }
     }
 
--- tree-avl/src/main/java/com/youtube/geekific/MainApp.java
+++ tree-avl/src/main/java/com/youtube/geekific/MainApp.java
@@ -35,7 +35,7 @@ public class MainApp {
         avlTree.insert(10).insert(2).insert(6).insert(8).insert(25).insert(18).insert(35).insert(15).insert(22).insert(42)
                 .insert(30).insert(40).insert(12).insert(17).insert(19).insert(24).insert(28).insert(33).insert(38);
 
-        avlTree.traverse();
+        avlTree.traverse("inorder");
 
         System.out.println("Max is: " + avlTree.getMax());
         System.out.println("Min is: " + avlTree.getMin());
@@ -45,7 +45,7 @@ public class MainApp {
 
         System.out.println("New Max is: " + avlTree.getMax());
 
-        avlTree.traverse();
+        avlTree.traverse("inorder");
 
     }
 
